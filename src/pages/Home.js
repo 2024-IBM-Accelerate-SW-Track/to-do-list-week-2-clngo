6eebd74b0ce008174e5430c86a687d5670741133
Colin Ngo <clngo654@gmail.com>
2024-06-16 20:08:02 -0700

    Finish Week2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,12 +13,35 @@ class Home extends Component {
   }
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
   // updates the state with the new list.
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: todos,
+
+    });
+};
   addTodo = (todo) => {
+
+
+
+    // Check if the todo item already exists
+    const exists = this.state.todos.find((t) => t.text === todo.text);
+    if (exists) {
+      return; // Exit the function if the todo already exists
+    }
+
+
+
     // In React, keys or ids in a list help identify which items have changed, been added or removed. Keys
     // should not share duplicate values.
     // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
+
+
+
     todo.id = Math.random();
     // Create a array that contains the current array and the new todo item
     let new_list = [...this.state.todos, todo];
@@ -33,10 +56,10 @@ class Home extends Component {
         <h1>Todo's </h1>
         {/* When passing the AddTodo component, addTodo is a prop that is used in the 
         AddTodo.js file when handling the submit */}
-        <AddTodo addTodo={this.addTodo} />
+        <AddTodo addTodo={this.addTodo} deleteTodo={this.deleteTodo}/>
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos } deleteToDo={this.deleteTodo}/>
       </div>
     );
   }
